8d0bfb7ec4641e605416a32116d1edcbcf240d7d
Héctor Miranda <43997408+H3cth0r@users.noreply.github.com>
2023-11-04 19:27:10 -0600

    Delete nvim directory

--- nvim/init.lua
+++
@@ -1,9 +0,0 @@
-require("my_config.plugins-setup")
-require("my_config.core.options")
-require("my_config.core.keymaps")
-require("my_config.core.colorscheme")
-require("my_config.plugins.comment")
-require("my_config.plugins.nvim-tree")
-require("my_config.plugins.telescope")
-require("my_config.plugins.nvim-cmp")
-
