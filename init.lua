688afecdd74ea979a9764d8eb43ba682dbf4ac03
Héctor Miranda <43997408+H3cth0r@users.noreply.github.com>
2023-11-04 19:32:46 -0600

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,9 @@
+require("my_config.plugins-setup")
+require("my_config.core.options")
+require("my_config.core.keymaps")
+require("my_config.core.colorscheme")
+require("my_config.plugins.comment")
+require("my_config.plugins.nvim-tree")
+require("my_config.plugins.telescope")
+require("my_config.plugins.nvim-cmp")
+
